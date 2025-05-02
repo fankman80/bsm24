bb6da40246bd43bc1330ab7b2db1f4ddc6e69d05
fankman80 <m.fankhauser@gmx.net>
2025-05-02 16:10:48 +0200

    Bugfixes 02.05.2025

--- bsm24/Services/Scale255To0Converter.cs
+++ bsm24/Services/Scale255To0Converter.cs
@@ -6,7 +6,6 @@ namespace bsm24.Services;
 
 public class Scale255To0Converter : IValueConverter
 {
-    // value kommt aus deiner ViewModel‑Property (0…255) → UI (0…1)
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (value is double d255)
@@ -17,15 +16,8 @@ public class Scale255To0Converter : IValueConverter
         return "0";
     }
 
-    // value kommt aus dem Entry als String (0…1) → ViewModel (0…255)
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        var str = value as string;
-        if (double.TryParse(str, NumberStyles.Any, culture, out var d01))
-        {            
-            // skaliere zurück auf 0…255
-            return Math.Clamp(d01 / 255, 0, 1);
-        }
-        return 0d;
+        throw new NotImplementedException("One-way converter only.");
     }
 }
--- bsm24/Services/Scale360To0Converter.cs
+++ bsm24/Services/Scale360To0Converter.cs
@@ -1,34 +1,23 @@
 ﻿#nullable disable
 
-using ExCSS;
 using System.Globalization;
 
 namespace bsm24.Services;
 
 public class Scale360To0Converter : IValueConverter
 {
-    // value kommt aus deiner ViewModel‑Property (0…360) → UI (0…1)
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (value is double d360)
         {
             // skaliere auf 0…1
-            var degree = (int)(d360 * 360);
-            return $"{degree}°";
+            return ((int)(d360 * 360)).ToString();
         }
-        return "0°";
+        return "0";
     }
 
-    // value kommt aus dem Entry als String (0…1) → ViewModel (0…360)
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        var str = value as string;
-        str = str.Replace("°", "").Trim();
-        if (double.TryParse(str, NumberStyles.Any, culture, out var d01))
-        {            
-            // skaliere zurück auf 0…360
-            return Math.Clamp(d01 / 360, 0, 1);
-        }
-        return 0d;
+        throw new NotImplementedException("One-way converter only.");
     }
 }
